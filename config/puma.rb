39161486f6ca9c99fa01f907ea3a8607b9a26013
Neil Johari <neil@johari.tech>
2020-09-08 12:40:48 -0400

    Fix Procfile

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,12 @@
-threads 1, 6
-workers 2
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
   require "active_record"
